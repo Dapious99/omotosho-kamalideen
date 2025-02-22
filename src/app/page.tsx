22258fce91fff29a9ac20142567cbf5801ae29ee
Kodewarlock <dapious99@gmail.com>
2025-02-22 15:16:59 +0100

    added footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,18 @@ import About from "@/components/About";
 import Experience from "@/components/Experience";
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
+import { currentYear } from "@/utils";
 
 export default function Home() {
   return (
-    <div className="bg-white">
+    <div className="bg-white ">
       <Hero />
       <About />
       <Projects />
       <Experience />
+      <footer className="text-[#C9A000] container mx-auto px-4 py-2 border-t border-t-[#C9A000]">
+        &#169; Copyright {currentYear} | Kodewarlock
+      </footer>
     </div>
   );
 }
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -96,3 +96,5 @@ export const experiences: Experience[] = [
     ],
   },
 ];
+
+export const currentYear = new Date().getFullYear();
